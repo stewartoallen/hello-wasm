9b5494ea8f6c0a05c8de3a08b06d4915b4ebb060
Stewart Allen <stewart.allen@gmail.com>
2020-04-29 22:38:01 -0400

    add some notes

--- hello.c
+++ hello.c
@@ -1,10 +1,13 @@
 /**
  * compile with: emcc hello.c -o hello.wasm -O3
- * decompile to .wat with: wasm2wat hello.wasm
+ * convert to readable .wat with: wasm2wat hello.wasm
  */
 
 #include <emscripten.h>
 
+/* required annotation to keep compiler from optimizing
+ * this function away. also prevents compiler from mangling
+ * function name on export making it easy to access from javascript */
 EMSCRIPTEN_KEEPALIVE
 int adder(int a, int b) {
 	return a + b;
@@ -22,6 +25,9 @@ int array_put(int p, int v) {
     return array[p] = v;
 }
 
+/* first param is pointer to shared memory position.
+ * memory is created by .wasm then accessible from
+ * javascript as an ArrayBuffer */
 EMSCRIPTEN_KEEPALIVE
 int reverse(unsigned char* p, int len) {
     for (int i = 0; i < len / 2; ++i) {
